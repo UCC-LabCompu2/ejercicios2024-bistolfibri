1e1a758196d37f1e6ec3e909c3b52c2520744640
bribistolfi <bribistolfi@gmail.com>
2024-05-20 10:51:49 -0300

    se realizaron ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -37,14 +37,19 @@ ConvetirUnidades = (id, valor) => {
        valpie = valor*3;
        valmetro = valor*36;
     }
-    document.getElementById("metro").value= valmetro;
-    document.getElementById("pulgada").value= valpulgada;
-    document.getElementById("pie").value = valpie;
-    document.getElementById("yarda").value= valyada;
+    document.getElementById("metro").value=Math.round(valmetro*100)/100 ;
+    document.getElementById("pulgada").value= Math.round(valpulgada*100)/100;
+    document.getElementById("pie").value = valpie.toFixed(2);
+    document.getElementById("yarda").value= valyada.toFixed(2);
 
 }
 
-
+/**
+ * convierte un valor en grados ingresado por el usuario en radines
+ * @method convertirGR
+ * @param {string} nombre = Id del elemento en el HTML
+ * @param {number} valor = valor ingresado por el usuario
+ */
 function convertirGR(id) {
     if (id==="grados") {
         let grat = document.getElementById("grados").value;
@@ -58,6 +63,12 @@ function convertirGR(id) {
 
     }
 }
+/**
+ * mostrar o ocultar un div
+ * @method mostrarocultar
+ * @param id {string}  = Id del elemento en el HTML
+
+ */
 
 let mostrarocultar = (valorMO) => {
     if (valorMO==="val_mostrar"){
@@ -66,4 +77,74 @@ let mostrarocultar = (valorMO) => {
     } else if (valorMO==="val_ocultar") {
         document.getElementById("divMO").style.display = 'none' ;
     }
+}
+/**
+ * permite sumar dos numeros ingresados por el usuario
+ * @method suma
+ */
+let suma = () => {
+    let num1, num2;
+    num1 = Number(document.getElementsByName("sum_num1")[0].value);
+    num2 = Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].value = num1 + num2;
+}
+/**
+ * permite restar dos numeros ingresados por el usuario
+ * @method resta
+ */
+let resta = () => {
+    let num1, num2;
+    num1 = Number(document.getElementsByName("res_num1")[0].value);
+    num2 = Number(document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].value = num1 - num2;
+}
+/**
+ * permite multiplicar dos numeros ingresados por el usuario
+ * @method prod
+ */
+let prod = () => {
+    let num1, num2;
+    num1 = Number(document.getElementsByName("mul_num1")[0].value);
+    num2 = Number(document.getElementsByName("mul_num2")[0].value);
+    document.getElementsByName("mul_total")[0].value = num1 * num2;
+}
+/**
+ * permite dividir dos numeros ingresados por el usuario
+ * @method div
+ */
+let div = () => {
+    let num1, num2;
+    num1 = Number(document.getElementsByName("div_num1")[0].value);
+    num2 = Number(document.getElementsByName("div_num2")[0].value);
+    document.getElementsByName("div_total")[0].value = num1 / num2;
+}
+/**
+ * permite concatenar una distancia y una unidad en la url antes de cargar la nueva pagina
+ * @method pasarpagina
+ */
+let pasarpagina = () => {
+    let cant, unid, urlcomp;
+    cant = document.getElementById( "distancia").value;
+    unid = document.getElementsByName( "unidades")[0].value;
+
+    console.log(cant);
+    console.log(unid);
+
+    urlcomp = "segundaweb.html#" + cant + "#" + unid;
+    window.open(urlcomp);
+}
+/**
+ * permite tomar los valores de distancia y unidad en la url y escribirlos en el input
+ * @method cargarvalores
+ */
+let cargarvalores =() => {
+    let cantidad, unidad, urlcompleta ;
+    urlcompleta = window.location.href;
+    console.log(urlcompleta);
+    urlcompleta = urlcompleta.split ( "#");
+    console.log(urlcompleta);
+    cantidad = urlcompleta[1];
+    unidad = urlcompleta[2];
+    document.getElementById("dist").value= cantidad + "" + unidad;
+
 }
